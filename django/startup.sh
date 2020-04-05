018fde9fdda01ca70466621c8d31ce5b31ce85c2
JC Clark <jc@saipantech.com>
2020-04-05 00:00:19 -0700

    fixes to go

---
+++ django/startup.sh
@@ -0,0 +1,10 @@
+cd /usr/src/app/django/mcsap
+
+./manage.py collectstatic --noinput
+./manage.py makemigrations
+./manage.py migrate
+
+#./manage.py runserver 0.0.0.0:8088
+
+cd /usr/src/app/django
+uwsgi --ini   uwsgi.ini 
--- project-data/django/webapps/webapps/settings.py
+++ project-data/django/webapps/webapps/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u!q@pxj^@^^u&gy5*sj_ww^r-5oi7@art(&b2#im(da1c1$412'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
