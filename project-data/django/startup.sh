8c3d44ea95f6b91c376eecedd9c38f2399ad0872
JC Clark <admin@mcsaipan.net>
2020-04-05 17:30:57 +1000

    current server state

--- project-data/django/startup.sh
+++ project-data/django/startup.sh
@@ -1,11 +1,10 @@
 cd /usr/src/app/django/webapps
 
-./manage.py runserver 0.0.0.0:8888
+#/manage.py collectstatic --noinput
+./manage.py makemigrations
+./manage.py migrate
 
+#./manage.py runserver 0.0.0.0:8088
 
-
-#./manage.py makemigrations
-#./manage.py migrate
-
-#./manage.py runserver 0.0.0.0:8888
-
+cd /usr/src/app/django
+uwsgi --ini   uwsgi.ini 
