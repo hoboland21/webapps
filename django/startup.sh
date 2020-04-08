5b2bcb7ae1841876b8b93636969abf76f2039b7b
JC Clark <jc@saipantech.com>
2020-04-08 07:30:01 -0700

    startup fixes

--- django/startup.sh
+++ django/startup.sh
@@ -1,10 +1,9 @@
-cd /usr/src/app/django/webapps
-
-./manage.py collectstatic --noinput
-./manage.py makemigrations
-./manage.py migrate
-
-#./manage.py runserver 0.0.0.0:8088
-
 cd /usr/src/app/django
-uwsgi --ini   uwsgi.ini 
+
+#webapps/manage.py collectstatic --noinput
+webapps/manage.py makemigrations
+webapps/manage.py migrate
+
+webapps/manage.py runserver 0.0.0.0:8888
+
+#uwsgi --ini   uwsgi.ini 
